6ed43be65b61edd79a8b862085cf95c2a8f72c27
Guido De Rosa <job@guidoderosa.net>
2009-07-03 19:34:37 +0200

    "smartly" compare partition tables

--- disk.rb
+++ disk.rb
@@ -58,10 +58,23 @@ class Disk
 
     diskdir = machinedir + "/" + disk.kernel_id
 
+    puts "OK I'M DOING THE ACTUAL RESTORE ;-)" 
+
+    return {:disk => disk, :state => :ok}
+
   end
 
   def compare_ptable(disk)
-    return false     
+
+    relation = proc do |vol1, vol2|
+      vol1.size	  == vol2.size	and
+      vol1.fstype == vol2.fstype and
+      vol1.pn	  == vol2.pn
+    end
+
+    return disk.volumes.length == \
+      disk.volumes.how_many_in_common_rel(@volumes, relation)
+
   end
 
 end

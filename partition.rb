d555dd8d38abc3dba59707ea42111b451f9b273c
Guido De Rosa <job@guidoderosa.net>
2010-01-07 18:47:03 +0000

    [WIP] DAR archiving

--- partition.rb
+++ partition.rb
@@ -4,8 +4,9 @@ class Partition
 
   attr_reader :dev, :fstype, :kernel_id, :pn, :size, :start, :end, :mountpoint
 
-  Mount_base = "/mnt/target"
+  Mount_base = "./mnt"
   Image_file_name = "img"
+  Archive_file_name = "files"
   
   def initialize(args) 
     @dev = args[:dev]
@@ -25,7 +26,6 @@ class Partition
     dest_file = nil
     dest_file_partial = nil
     cmd = nil
-    #FileUtils.mv(dest_file, dest_file + ".old") if File.exists?(dest_file)
 
     case @fstype
     
@@ -46,9 +46,36 @@ class Partition
     end
 
     # rename dest_file.partial to dest_file only on success
-    if system("sudo #{cmd}")  
-      FileUtils.mv(dest_file_partial, dest_file)
-    end
+    #if system("sudo #{cmd}")  
+    #  FileUtils.mv(dest_file_partial, dest_file)
+    #end
+    puts "fake cloning!"
+    gets
+
+    # Now, make a files archive (use DAR http://dar.linux.free.fr/ )  
+    dest_archive = File.expand_path(dir + '/' + Archive_file_name)
+    mount_type = case @fstype
+                 when "vfat", "fat", "fat32", "fat16", "msdos", "msdosfs"
+                   'vfat'
+                 when "ntfs"
+                   'ntfs' # I believe kernel-based RO NTFS is fast
+                 else
+                   @fstype
+                 end
+
+    mount({:options => 'ro', :type => mount_type}) unless mounted?
+    puts "#@dev -> #@mountpoint -> #{dest_archive}"
+    system <<EOF
+sudo \
+  dar -v -z1 -K "blowfish:" -M --no-warn -R #@mountpoint -c #{dest_archive} \
+  --alter=no-case \
+  -X hiberfil.sys -X pagefile.sys -P "System Volume Information" -P MSOCache \
+  -Z "*.cab" -Z "*.zip" -Z "*.gz" -Z "*.bz2" -Z "*.lz*" -Z "*.jar" \
+  -Z "*.png" -Z "*.gif" -Z "*.jp*g" \
+  -Z "*.mp*" -Z "*.avi" -Z "*.mov" -Z "*.wm*"
+EOF
+    gets
+    umount
 
   end
 
@@ -82,10 +109,13 @@ class Partition
     return false
   end
 
-  def mount(mountpoint="#{Mount_base}/#@dev")
-    File.makedirs(mountpoint) unless File.directory?(mountpoint)
-    if system("mount #@dev #{mountpoint}") 
-      @mountpoint = mountpoint
+  def mount(h)
+    mountpoint = h[:mountpoint] || "#{Mount_base}/#{File.basename @dev}"
+    type = h[:type] || 'auto'
+    options = h[:options] ? "-o #{h[:options]}" : ""
+    Dir.mkdir(mountpoint) unless File.directory?(mountpoint)
+    if system("sudo mount -t #{type} #@dev #{mountpoint} #{options}") 
+      @mountpoint = File.expand_path mountpoint
       return true
     else
       return false
@@ -94,7 +124,7 @@ class Partition
 
   def umount
     if mounted?
-      if system("umount #@dev 2> /dev/null")
+      if system("sudo umount #@dev 2> /dev/null")
         @mountpoint = nil
         return true
       else
